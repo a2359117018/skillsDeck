45a9c7ac39d21f544ee0c1d76c98eb051729f8d6
a2359117018 <2359117018@qq.com>
2026-05-04 15:05:31 +0800

    feat(ui): add AppLoading global overlay component

---
+++ src/renderer/src/components/common/AppLoading.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { NSpin } from 'naive-ui'
+
+defineProps<{ show: boolean }>()
+</script>
+
+<template>
+  <Transition name="fade">
+    <div v-if="show" class="app-loading-overlay">
+      <NSpin size="large" />
+    </div>
+  </Transition>
+</template>
+
+<style scoped>
+.app-loading-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 9999;
+}
+</style>
